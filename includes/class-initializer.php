375ce140512f3ac806223cf5dc3d67bda4b61bdf
Miguel Peixe <miguel.peixe@automattic.com>
2026-04-01 13:19:31 -0300

    first commit

---
+++ includes/class-initializer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Newspack Lite Site plugin initialization.
+ *
+ * @package Newspack
+ */
+
+namespace Newspack_Lite_Site;
+
+/**
+ * Class to handle the plugin initialization.
+ */
+class Initializer {
+
+	/**
+	 * Runs the initialization.
+	 */
+	public static function init() {
+		// Register hooks and features here.
+	}
+}
---
+++ newspack-lite-site.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: Newspack Lite Site
+ * Description: Lightweight Newspack site configuration.
+ * Version: 0.1.0
+ * Author: Automattic
+ * Author URI: https://newspack.com/
+ * License: GPL3
+ * Text Domain: newspack-lite-site
+ * Domain Path: /languages/
+ *
+ * @package newspack-lite-site
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Define NEWSPACK_LITE_SITE_PLUGIN_DIR.
+if ( ! defined( 'NEWSPACK_LITE_SITE_PLUGIN_DIR' ) ) {
+	define( 'NEWSPACK_LITE_SITE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+// Define NEWSPACK_LITE_SITE_PLUGIN_FILE.
+if ( ! defined( 'NEWSPACK_LITE_SITE_PLUGIN_FILE' ) ) {
+	define( 'NEWSPACK_LITE_SITE_PLUGIN_FILE', __FILE__ );
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+Newspack_Lite_Site\Initializer::init();
+
+// Load language files.
+add_action(
+	'init',
+	function () {
+		load_plugin_textdomain( 'newspack-lite-site', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+);
