77c292c4bdf00981913a08baafc04c6f6dac0453
Rishabh Gupta <rishabh.gupta@rtcamp.com>
2026-05-04 15:33:44 +0530

    refactor: extract post list logic into a separate template partial

--- templates/archive.php
+++ templates/archive.php
@@ -40,49 +40,7 @@ namespace Newspack_Lite_Site;
 	<h1><?php bloginfo( 'name' ); ?></h1>
 	<hr class="separator">
 	<ul class="post-list">
-	<?php
-	$query_args = [
-		'posts_per_page' => Lite_Site::get_number_of_posts(),
-		'post_status'    => 'publish',
-	];
-
-	$categories = Lite_Site::get_categories();
-	if ( ! empty( $categories ) ) {
-		$query_args['category__in'] = $categories;
-	}
-
-	// Render sticky posts prominently at the top.
-	$sticky_post_ids_option = get_option( 'sticky_posts' );
-	$sticky_post_ids        = [];
-	$sticky_posts           = [];
-	if ( ! empty( $sticky_post_ids_option ) ) {
-		$sticky_post_ids = array_values( $sticky_post_ids_option );
-		$sticky_posts    = get_posts(
-			[
-				'post__in' => $sticky_post_ids,
-			]
-		);
-	}
-
-	$recent_posts = get_posts( $query_args );
-
-	$all_posts = array_merge( $sticky_posts, $recent_posts );
-
-	$all_posts = array_unique( $all_posts, SORT_REGULAR );
-
-	$all_posts = array_slice( $all_posts, 0, Lite_Site::get_number_of_posts() );
-
-	foreach ( $all_posts as $current_post ) {
-		$is_sticky = in_array( $current_post->ID, $sticky_post_ids, true );
-		printf(
-			'<li>%s<a href="%s">%s</a>%s</li>',
-			$is_sticky ? '<h3>' : '',
-			esc_url( Lite_Site::get_lite_page_url( $current_post ) ),
-			esc_html( $current_post->post_title ),
-			$is_sticky ? '</h3>' : ''
-		);
-	}
-	?>
+		<?php require __DIR__ . '/post-list.php'; ?>
 	</ul>
 	<?php
 	$footer_html = Lite_Site::get_footer_html();
